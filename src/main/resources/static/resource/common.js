d9ee11ecc5028af4e6e1285e5e6d8657682942e4
JANG-MINYOUNG <wkdalsdudqq@gmail.com>
2024-02-29 12:59:44 +0900

    메인UI 구현중

---
+++ src/main/resources/static/resource/common.js
@@ -0,0 +1,33 @@
+$('select[data-value]').each(function(index, el) {
+	const $el = $(el);
+
+	defaultValue = $el.attr('data-value').trim();
+
+	if (defaultValue.length > 0) {
+		$el.val(defaultValue);
+	}
+	
+});
+
+function Mobile_TopBar__init() {
+  $(".mobile_top_bar .btn_toggle").click(function () {
+    $(this).toggleClass("active");
+  });
+}
+
+function TopBar__init() {
+  $(".top_bar .menu_box_1 > ul > li").mouseover(function () {
+    var $height = $(this).find("> ul").height();
+
+    var height_plus = $height + 50;
+
+    $(".bg").css("height", height_plus + "px");
+  });
+
+  $(".top_bar .menu_box_1 > ul > li").mouseleave(function () {
+    $(".bg").css("height", "0px");
+  });
+}
+
+TopBar__init();
+Mobile_TopBar__init();
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<c:set var="pageTitle" value=""></c:set>
+<%@ include file="../common/head.jspf"%>
+
+<div class="home-box"></div>
+<div class="img-box"><img src="https://cdn.chungnamilbo.co.kr/news/photo/202208/673955_257859_2551.jpg" alt="">
+</div>
+<%@ include file="../common/foot.jspf"%>
